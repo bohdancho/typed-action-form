751bebb2182669ca69228aea2a60863848fc006c
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-04-24 21:12:51 +0200

    typedActionForm PoC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,44 @@
+import type { TypedFormData, TypedFormDataValue } from "./typed-form-data";
+
+const form = typedActionForm("name", "age");
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <form
+        className="flex flex-col gap-2"
+        action={async (f: FormData) => {
+          "use server";
+
+          const formData = f as typeof form.$infer;
+          const name = formData.get("name");
+          const age = formData.get("age");
+          console.log("Name:", name);
+          console.log("Age:", age);
+
+          const invalid = formData.get("name1");
+        }}
+      >
+        <label>
+          <p>Name:</p>
+          <input {...form.name} className="text-black" />
+        </label>
+        <label>
+          <p>Age:</p>
+          <input {...form.age} className="text-black" />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </main>
   );
 }
+
+function typedActionForm<T extends string>(...args: T[]) {
+  return Object.fromEntries(args.map((name) => [name, { name }])) as {
+    [K in T]: { name: K };
+  } & { readonly $infer: InferFormData<{ [K in T]: K }> };
+}
+
+type InferFormData<T> =
+  T extends Record<infer U, unknown>
+    ? TypedFormData<Record<U, TypedFormDataValue>>
+    : never;
