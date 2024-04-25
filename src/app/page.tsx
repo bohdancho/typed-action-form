ca16e6a75e829d310e2a4eb5196ba1c0d4ffcac8
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-04-25 18:48:07 +0200

    move lib code to a separate directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import type { TypedFormData, TypedFormDataValue } from './typed-form-data'
+import { typedActionForm } from '@/lib'
 
 const form = typedActionForm('name', 'age')
 export default function Home() {
@@ -30,31 +30,3 @@ export default function Home() {
     </main>
   )
 }
-
-function typedActionForm<
-  TFields extends string,
-  TInputsProps extends InputProps<TFields>,
-  TFormData extends InferFormData<TFields>,
->(...args: TFields[]) {
-  const inputsProps = Object.fromEntries(args.map((name) => [name, { name }])) as TInputsProps
-
-  function $inferFormData(formData: FormData) {
-    return formData as unknown as TFormData
-  }
-
-  function $action(action: (formData: TFormData) => void) {
-    return async (formData: FormData) => {
-      'use server'
-
-      return action(formData as unknown as TFormData)
-    }
-  }
-
-  return { ...inputsProps, $inferFormData, $action }
-}
-
-type InputProps<TFields extends string> = {
-  [K in TFields]: { name: K }
-}
-
-type InferFormData<T extends string> = TypedFormData<Record<T, TypedFormDataValue>>
